866425a8ffdc85ea3dce3f0506b2d4ef476dfa1e
hgccgh <94579246+hgccgh@users.noreply.github.com>
2021-12-02 23:36:27 +0800

    Delete src directory

--- src/CatCafe.java
+++
@@ -1,6 +0,0 @@
-import java.lang.*;
-public interface CatCafe {
-     void purchase(String name,double price) throws InsufficientBalanceException;
-    void treat(String... customer);
-     void rest();
-}
